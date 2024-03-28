ba408491a9626bd65f5d6ccd378a0abf6349c47c
mateusavila <mateus@mateusavila.com.br>
2024-03-28 16:20:23 -0300

    fix: missing logo

--- components/FamousGenerals.vue
+++ components/FamousGenerals.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import type { FamousGenerals } from '~/types'
+import type { FamousGenerals } from '~/utils'
 interface ComponentInterface {
   list: FamousGenerals[],
   title: string
