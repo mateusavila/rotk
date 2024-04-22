68bb8b7d2a805f51f6bea792f8d49ceed1781bac
mateusavila <mateus@mateusavila.com.br>
2024-04-22 12:01:44 -0300

    feat: cors and filter

--- server/api/selects.get.ts
+++ server/api/selects.get.ts
@@ -1,11 +1,26 @@
+import { like } from "drizzle-orm"
 import { generals } from "../schemas"
 
-export default defineCachedEventHandler(async () => {
+export default defineEventHandler(async (event) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*'
+  }
+  setHeaders(event, headers)
+  const query = getQuery(event)
+  const { term, _type, q } = query
   const client = useTurso()
+  if (!term) {
+    const results = await client.select({
+      text: generals.name,
+      id: generals.slug
+    }).from(generals).orderBy(generals.name).all()
+    return { results }
+  }
+
   const results = await client.select({
     text: generals.name,
     id: generals.slug
-  }).from(generals).orderBy(generals.name).all()
-
+  }).from(generals).where(like(generals.name, `${term}%`)).orderBy(generals.name).all()
   return { results }
-}, { maxAge: 60 * 60 * 24 * 400 })
+
+})
